e666d2f7955cf714ed252661365e251f75557732
omarsalah25 <55956352+omarsalah25@users.noreply.github.com>
2023-11-09 16:14:28 +0200

    up

--- mail.php
+++ mail.php
@@ -2,9 +2,9 @@
 
 if ( $_POST ) {
 
-	$frm_name  = 'John Doe'; // your name
-	$recepient = 'john@example.com'; // your e-mail
-	$sitename  = 'Kinsey HTML5 Template'; // your site name
+	$frm_name  = 'Black Brick'; // your name
+	$recepient = 'maherfared@gmail.com'; // your e-mail
+	$sitename  = 'Black brick Website'; // your site name
 	$subject   = "New contact from \"$sitename\""; // subject template
 
 	$name  = trim( $_POST['visitor_name'] );
